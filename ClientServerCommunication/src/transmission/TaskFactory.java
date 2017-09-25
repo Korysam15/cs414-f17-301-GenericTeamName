e8c072baa18cfda009532853f89c3dbc97923d05
pflagert <pflagert@gmail.com>
2017-09-25 11:16:20 -0600

    Added use of ReadUtils

--- ClientServerCommunication/src/transmission/TaskFactory.java
+++ ClientServerCommunication/src/transmission/TaskFactory.java
@@ -3,7 +3,11 @@
  */
 package transmission;
 
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
+import java.io.IOException;
+
+import transmission.util.ReadUtils;
 
 /**
  * @author pflagert
@@ -25,14 +29,30 @@ public final class TaskFactory {
 		return t;
 	}
 	
-	public Task createTaskFromBytes(byte[] b) {
-		Task t = null;
-		
+	public Task createTaskFromBytes(byte[] bytes) throws IOException {
+		ByteArrayInputStream ba = ReadUtils.getByteInputStream(bytes);
+		DataInputStream din = ReadUtils.getDataInputStream(ba);
+		Task t = createTaskFromDataInputStream(din);
 		return t;
 	}
 	
-	public Task createTaskFromDataInputStream(DataInputStream din) {
+	public Task createTaskFromDataInputStream(DataInputStream din) throws IOException {
 		Task t = null;
+		
+		int taskCode = din.readInt();
+		
+		switch(taskCode) {
+		case TaskConstents.TASK: 
+			
+			break;
+		case TaskConstents.FORWARD_TASK:
+			
+			break;
+			
+		case TaskConstents.REGISTER_TASK:
+			
+			break;
+		}
 		
 		return t;
 	}
