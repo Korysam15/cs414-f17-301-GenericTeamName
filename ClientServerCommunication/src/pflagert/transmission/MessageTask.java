6d1db6642978486cf1630addded75c667d0372e2
Korysam15 <korysam@comcast.net>
2017-10-20 09:42:43 -0600

    Added two tasks to task factory

--- ClientServerCommunication/src/pflagert/transmission/MessageTask.java
+++ ClientServerCommunication/src/pflagert/transmission/MessageTask.java
@@ -12,35 +12,33 @@ import pflagert.transmission.util.WriteUtils;
  * @author pflagert
  * Just a Test for the server / client
  */
-public class TestTask extends Task {
+public class MessageTask extends Task {
 
 	private String msg;
 
-	public TestTask() {
+	public MessageTask() 
+	{
 		this("");
 	}
 	
-	public TestTask(String msg) {
+	public MessageTask(String msg) 
+	{
 		super();
 		this.msg = msg;
 	}
 	
-	public TestTask(DataInputStream din) throws IOException {
-		/* assume this is done already */
-		// int someTaskCode = din.readInt() 
-		
+	public MessageTask(DataInputStream din) throws IOException 
+	{
 		this.msg = ReadUtils.readString(din);
-	
-		
-		/* let the caller close the DataInputStream */
-		// din.close() 
 	}
 
-	public int getTaskCode() {
+	public int getTaskCode() 
+	{
 		return TaskConstents.TEST_TASK;
 	}
 
-	public byte[] toByteArray() throws IOException {
+	public byte[] toByteArray() throws IOException 
+	{
 		ByteArrayOutputStream bs = WriteUtils.getByteOutputStream();
 		DataOutputStream dout = WriteUtils.getDataOutputStream(bs);
 		dout.writeInt(getTaskCode());
@@ -48,16 +46,14 @@ public class TestTask extends Task {
 		return WriteUtils.getBytesAndCloseStreams(bs,dout);
 	}
 
-	public String toString() {
+	public String toString() 
+	{
 		return "Taskcode: " + TaskConstents.TEST_TASK + " Message: " + msg ;
 	}
 
-	public void run() {
-		System.out.println("It worked.... Kinda, I think.  I guess we will see..." );
-		for(int i=0; i<10; i++) {
-			System.out.println(msg);
-		}
-		System.out.println("Well did it work????");
+	public void run() 
+	{
+		System.out.println(msg);
 	}
 
 }
