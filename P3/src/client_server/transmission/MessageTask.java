020bee6283cf3cc64dbcb8418277387cadf4b7ae
pflagert <pflagert@gmail.com>
2017-10-24 09:36:59 -0600

    MessageTask: now uses a Player's Console

--- P3/src/client_server/transmission/MessageTask.java
+++ P3/src/client_server/transmission/MessageTask.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 
 import client_server.transmission.util.ReadUtils;
 import client_server.transmission.util.WriteUtils;
+import console.AbstractConsole;
+import user.ActivePlayer;
+import user.Player;
 
 /**
  * @author pflagert
@@ -14,22 +17,46 @@ import client_server.transmission.util.WriteUtils;
  */
 public class MessageTask extends Task {
 
+	public static final int DEFAULT=4;
+	public static final int NOTICE=3;
+	public static final int WARNING=2;
+	public static final int ERROR=1;
+	
 	private String msg;
-
+	private int type;
+	
 	public MessageTask() 
 	{
-		this("");
+		this("",DEFAULT);
 	}
 	
 	public MessageTask(String msg) 
 	{
+		this("",DEFAULT);
+	}
+	
+	public MessageTask(String msg, int type)
+	{
 		super();
 		this.msg = msg;
+		setType(type);
+	}
+	
+	private void setType(int type) {
+		switch(type) {
+		case DEFAULT: case NOTICE: case WARNING: case ERROR:
+			this.type = type;
+			return;
+		default:
+			this.type = DEFAULT;
+		}
 	}
 	
 	public MessageTask(DataInputStream din) throws IOException 
 	{
-		this.msg = ReadUtils.readString(din);
+		msg = ReadUtils.readString(din);
+		type = din.readInt();
+		setType(type);
 	}
 
 	public int getTaskCode() 
@@ -43,6 +70,7 @@ public class MessageTask extends Task {
 		DataOutputStream dout = WriteUtils.getDataOutputStream(bs);
 		dout.writeInt(getTaskCode());
 		WriteUtils.writeString(msg,dout);
+		dout.writeInt(type);
 		return WriteUtils.getBytesAndCloseStreams(bs,dout);
 	}
 
@@ -53,7 +81,28 @@ public class MessageTask extends Task {
 
 	public void run() 
 	{
-		System.out.println(msg);
+		Player player = ActivePlayer.getInstance();
+		if(player != null) {
+			AbstractConsole console = player.getConsole();
+			if(console != null) {
+				switch(type) {
+				case DEFAULT:
+					console.display(msg);
+					break;
+				case NOTICE:
+					console.notice(msg);
+					break;
+				case WARNING:
+					console.warning(msg);
+					break;
+				case ERROR:
+					console.error(msg);
+					break;
+				}
+			} else {
+				System.out.println(msg);
+			}
+		}
 	}
 
 }
