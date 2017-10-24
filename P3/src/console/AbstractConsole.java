dbd551ce84f45cc4617faf1033c8d5045fe6f0f4
pflagert <pflagert@gmail.com>
2017-10-24 08:46:16 -0600

    AbstractConsole: removed unused imports

--- P3/src/console/AbstractConsole.java
+++ P3/src/console/AbstractConsole.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.InterruptedIOException;
-import java.io.OutputStream;
 import java.io.PrintStream;
 
 import user.Player;
