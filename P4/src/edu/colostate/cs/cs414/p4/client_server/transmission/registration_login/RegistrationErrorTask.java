646ca92bebf8b827fa1daed3f2e9592b99b930ce
pflagert <pflagert@gmail.com>
2017-10-25 10:18:59 -0600

    Added LoginErrorTask and RegistrationErrorTask

---
+++ P4/src/edu/colostate/cs/cs414/p4/client_server/transmission/registration_login/RegistrationErrorTask.java
@@ -0,0 +1,27 @@
+package edu.colostate.cs.cs414.p4.client_server.transmission.registration_login;
+
+import java.io.IOException;
+
+import edu.colostate.cs.cs414.p4.client_server.transmission.Task;
+
+public class RegistrationErrorTask extends Task {
+
+	@Override
+	public int getTaskCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public byte[] toByteArray() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
