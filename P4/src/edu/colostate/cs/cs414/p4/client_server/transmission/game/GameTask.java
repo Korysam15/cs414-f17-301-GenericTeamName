aedf11beab28582d833e5108bd3240777c10345b
pflagert <pflagert@gmail.com>
2017-11-02 10:10:29 -0600

    Added Interface GameTask.

---
+++ P4/src/edu/colostate/cs/cs414/p4/client_server/transmission/game/GameTask.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package edu.colostate.cs.cs414.p4.client_server.transmission.game;
+
+import edu.colostate.cs.cs414.p4.client_server.transmission.game.invite.AcceptInviteTask;
+import edu.colostate.cs.cs414.p4.client_server.transmission.game.invite.InviteTask;
+import edu.colostate.cs.cs414.p4.client_server.transmission.game.invite.RejectInviteTask;
+
+/**
+ * @author pflagert
+ * A simple interface to allow {@link Task}s related to game play / creation to
+ * distinguish themselves from other Tasks. 
+ */
+public interface GameTask {
+	
+	/**
+	 * The value in which a Game's ID should be in the following tasks:
+	 * {@link InviteTask}
+	 * {@link RejectInviteTask}
+	 * {@link AcceptInviteTask}
+	 */
+	public static final int UNASSIGNED_GAME_ID = Integer.MIN_VALUE;
+	
+	/**
+	 * Returns the identifier for the game that an instance of GameTask is referring to.
+	 * @return Either the unique identifier assigned to the game, 
+	 * or {@link #UNASSIGNED_GAME_ID}
+	 */
+	public int getGameID();
+}
