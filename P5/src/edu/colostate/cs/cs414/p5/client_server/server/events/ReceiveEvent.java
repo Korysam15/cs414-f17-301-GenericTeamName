717658b3c362188d88f7884833f5f87e870bf5a9
pflagert <pflagert@gmail.com>
2017-12-03 10:35:42 -0700

    Added Logger to Receive / Send Events

--- P5/src/edu/colostate/cs/cs414/p5/client_server/server/events/ReceiveEvent.java
+++ P5/src/edu/colostate/cs/cs414/p5/client_server/server/events/ReceiveEvent.java
@@ -2,10 +2,11 @@ package edu.colostate.cs.cs414.p5.client_server.server.events;
 
 import java.io.IOException;
 
+import edu.colostate.cs.cs414.p5.client_server.logger.Logger;
 import edu.colostate.cs.cs414.p5.client_server.server.session.ClientSession;
 
 public class ReceiveEvent implements Event{
-
+	private static final Logger LOG = Logger.getInstance();
 	private ClientSession client;
 	
 	public ReceiveEvent(ClientSession client) {
@@ -17,8 +18,7 @@ public class ReceiveEvent implements Event{
 		try {
 			client.receive();
 		} catch (IOException e) {
-			System.out.println("Receive failed");
-			e.printStackTrace();
+			LOG.error("Receive Event failed due to IOException: " + e.getMessage());
 		}
 	}
 
