d3a52ec95f51ff7fcc12de1f1d10c4885ccf5640
pflagert <pflagert@gmail.com>
2017-12-03 13:28:58 -0700

    Updated GetProfileTask.java to use ProfileManager

--- P5/src/edu/colostate/cs/cs414/p5/client_server/transmission/profile/GetProfileTask.java
+++ P5/src/edu/colostate/cs/cs414/p5/client_server/transmission/profile/GetProfileTask.java
@@ -4,33 +4,35 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+import edu.colostate.cs.cs414.p5.client_server.logger.Logger;
+import edu.colostate.cs.cs414.p5.client_server.server.profile_manager.ProfileManager;
+import edu.colostate.cs.cs414.p5.client_server.server.session.SessionManager;
 import edu.colostate.cs.cs414.p5.client_server.transmission.Task;
 import edu.colostate.cs.cs414.p5.client_server.transmission.TaskConstents;
-import edu.colostate.cs.cs414.p5.client_server.transmission.util.ForwardTask;
+import edu.colostate.cs.cs414.p5.client_server.transmission.util.MessageTask;
 import edu.colostate.cs.cs414.p5.client_server.transmission.util.ReadUtils;
 import edu.colostate.cs.cs414.p5.client_server.transmission.util.WriteUtils;
-import edu.colostate.cs.cs414.p5.user.ActivePlayer;
-import edu.colostate.cs.cs414.p5.user.Player;
+import edu.colostate.cs.cs414.p5.user.Profile;
 
 public class GetProfileTask extends ProfileTask {
 
 	private String playerWhoHasIt;
-	
+
 	public GetProfileTask(String playerWhoWantsIt, String playerWhoHasIt) {
 		super(playerWhoWantsIt);
 		this.playerWhoHasIt = playerWhoHasIt;
 	}
-	
+
 	public GetProfileTask(DataInputStream din) throws IOException {
 		super(din);
 		playerWhoHasIt = ReadUtils.readString(din);
 	}
-	
+
 	@Override
 	public int getTaskCode() {
 		return TaskConstents.GET_PROFILE_TASK;
 	}
-	
+
 	@Override
 	public void writeBytes(DataOutputStream dout) throws IOException {
 		super.writeBytes(dout);
@@ -44,18 +46,26 @@ public class GetProfileTask extends ProfileTask {
 
 	@Override
 	public void run() {
-		Player player = ActivePlayer.getInstance();
-		if(player != null) {
-			String profile = player.getProfile().toString();
-			Task display = new DisplayProfileTask(profile);
-			Task forward = new ForwardTask(playerWhoHasIt,display,getRequester());
-			try {
-				player.getClient().sendToServer(forward);
-			} catch (IOException e) {
+		SessionManager server = SessionManager.getInstance();
+		ProfileManager profileManager = ProfileManager.getInstance();
 
+		if(server != null && server != null) {
+			Task response;
+			Profile profile = profileManager.getProfile(playerWhoHasIt);
+			if(profile != null) {
+				response = new DisplayProfileTask(profile.toString());
+			} else {
+				Logger.getInstance().info(super.getRequester() + " requested to see a profile for: " +
+						playerWhoHasIt + " but that profile does not exists.");
+				response = new MessageTask(playerWhoHasIt + " does not appear to have a profile...", MessageTask.ERROR);
 			}
+			server.sendToClient(response, super.getRequester());
+		} else {
+			Logger.getInstance().error("Failed to retrieve " + playerWhoHasIt +
+					" profile for " + super.getRequester() + ".\n" + 
+					" This error is due to the SessionManager or ProfileManager being null.");
 		}
-
 	}
 
 }
+
