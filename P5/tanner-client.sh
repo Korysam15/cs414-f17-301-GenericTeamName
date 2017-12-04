a7505fe181f24b6117d8ee4582067d2b6c7082dc
Korysam15 <korysam@comcast.net>
2017-12-03 23:44:10 -0700

    Added docs to github

---
+++ P5/tanner-client.sh
@@ -0,0 +1,14 @@
+BIN="bin/"
+
+PACKAGE="edu.colostate.cs.cs414.p5.main"
+
+MAIN_CLASS="PlayerMain"
+
+ADDR="76.120.120.62"
+
+PORT=54350
+
+
+cd $BIN
+clear
+java -cp . $PACKAGE.$MAIN_CLASS $ADDR $PORT
